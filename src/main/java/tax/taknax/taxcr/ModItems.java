920b7d82dd2bceb71672639a1fb709735bc63cf0
Daripher <saelver.serbek@gmail.com>
2021-09-11 20:42:46 +0500

    Added universal code for creative tabs

--- src/main/java/tax/taknax/taxcr/ModItems.java
+++ src/main/java/tax/taknax/taxcr/ModItems.java
@@ -3,21 +3,25 @@ package tax.taknax.taxcr;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import tax.taknax.api.tab.TaxCreativeTab;
 import tax.taknax.taxcr.common.items.ItemGuideBook;
 
 @Mod.EventBusSubscriber(modid = GuideBookMod.MODID)
 public class ModItems
 {
+	public static CreativeTabs taxTab;
 	public static Item guideBook;
 	
 	public static void init()
 	{
-		guideBook = new ItemGuideBook("cryptex").setCreativeTab(CreativeTabs.MISC);
+		initCreativeTab();
+		guideBook = new ItemGuideBook("cryptex").setCreativeTab(taxTab);
 	}
 	
 	@SubscribeEvent
@@ -30,6 +34,19 @@ public class ModItems
 	public static void registerRenders(ModelRegistryEvent event)
 	{
 		registerRender(guideBook);
+	}
+	
+	private static void initCreativeTab()
+	{
+		for (CreativeTabs tab : CreativeTabs.CREATIVE_TAB_ARRAY)
+		{
+			if (tab instanceof TaxCreativeTab)
+			{
+				return;
+			}
+		}
+		
+		taxTab = new TaxCreativeTab(() -> new ItemStack(guideBook));
 	}
 	
 	private static void registerRender(Item item)
