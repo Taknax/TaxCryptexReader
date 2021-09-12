008404cab40ea2e58d250cf53cd84c1bd5617052
Daripher <saelver.serbek@gmail.com>
2021-09-12 20:50:14 +0500

    Used creative tab from API

--- src/main/java/tax/taknax/taxcr/ModItems.java
+++ src/main/java/tax/taknax/taxcr/ModItems.java
@@ -1,27 +1,23 @@
 package tax.taknax.taxcr;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import tax.taknax.api.tab.TaxCreativeTab;
 import tax.taknax.taxcr.common.items.ItemGuideBook;
+import tax.taknax.taxlc.api.TaxAPI;
 
 @Mod.EventBusSubscriber(modid = GuideBookMod.MODID)
 public class ModItems
 {
-	public static CreativeTabs taxTab;
 	public static Item guideBook;
 	
 	public static void init()
 	{
-		initCreativeTab();
-		guideBook = new ItemGuideBook("cryptex").setCreativeTab(taxTab);
+		guideBook = new ItemGuideBook("cryptex").setCreativeTab(TaxAPI.TAX_CREATIVE_TAB);
 	}
 	
 	@SubscribeEvent
@@ -34,19 +30,6 @@ public class ModItems
 	public static void registerRenders(ModelRegistryEvent event)
 	{
 		registerRender(guideBook);
-	}
-	
-	private static void initCreativeTab()
-	{
-		for (CreativeTabs tab : CreativeTabs.CREATIVE_TAB_ARRAY)
-		{
-			if (tab instanceof TaxCreativeTab)
-			{
-				return;
-			}
-		}
-		
-		taxTab = new TaxCreativeTab(() -> new ItemStack(guideBook));
 	}
 	
 	private static void registerRender(Item item)
